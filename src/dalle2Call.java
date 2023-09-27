5f8862155ccaaeaebb1c309f266508d8d339524c
Hong-ut <7594hsj@gmail.com>
2023-09-27 17:07:43 -0400

    finished dalle2 call, intJCohere, and added lib directory for jar files

---
+++ src/dalle2Call.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.List;
+import com.intellijava.core.controller.RemoteImageModel;
+import com.intellijava.core.model.input.ImageModelInput;
+
+
+public class dalle2Call {
+
+    public static void main(String[] args) {
+
+        System.out.println("Start calling the API!");
+
+        try {
+
+            // get the api key from https://openai.com/api/
+            String apiKey = "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";
+            /******************************/
+            /** 2- Call image generation **/
+            /******************************/
+            tryTheImageModel(apiKey);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void tryTheImageModel(String apiKey) throws IOException {
+        // initiate the remote image model wrapper
+        RemoteImageModel imageModel = new RemoteImageModel(apiKey, "openai");
+
+        // prepare the input parameters
+        ImageModelInput imageInput = new ImageModelInput.Builder("teddy writing a blog in times square")
+                .setNumberOfImages(2).setImageSize("1024x1024").build();
+
+        // call the model
+        List<String> images = imageModel.generateImages(imageInput);
+
+        // print images links
+        System.out.println("Images links:");
+        for (String image : images) {
+            System.out.println(image);
+        }
+    }
+}
+
+
+
